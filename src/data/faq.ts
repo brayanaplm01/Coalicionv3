53c33e52f4a4d1f6dbf08b41128a8e163f23c86f
brayanaplm01 <apazalamas19@gmail.com>
2025-08-15 10:14:26 -0400

    Fix comentarios actualizados

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -27,7 +27,7 @@ export const faqData: FAQItem[] = [
   {
     id: 4,
     question: "Distribución de escaños a nivel nacional",
-    answer: "Un escaño es un puesto en la Asamblea Legislativa que representa a un grupo de personas. Cuantos más escaños tenga un partido, más poder tienen para tomar decisiones y hacer leyes. En una elección, los partidos y candidatos compiten para ganar estos escaños y así decidir sobre el futuro del país. La distribución de escaños la realiza el órgano electoral en base al número de habitantes de acuerdo al último Censo Nacional. Se distribuyen en base al procedimiento de asignación que considera los principios de igualdad territorial, equidad poblacional departamental, proporcionalidad y plurinacionalidad.",
+    answer: "Un escaño es un puesto en la Asamblea Legislativa que representa a un grupo de personas. Cuantos más escaños tenga un partido, más poder tienen para tomar decisiones y hacer leyes. En una elección, los partidos y candidatos compiten para ganar estos escaños y así decidir sobre el futuro del país. La distribución de escaños la realiza el órgano electoral en base al número de habitantes de acuerdo al último Censo Nacional. Se distribuyen en base al procedimiento de asignación que considera los principios de igualdad territorial, equidad poblacional departamental, proporcionalidad y plurinacionalidad. Escaños en la Cámara de Senadores: 36 senadores 4 por departamento. Principio de igualdad territorial, cada departamento independientemente de su población elige 4 senadores en votos acumulativos obtenidos por las candidaturas a presidenta o presidente, bajo distribución proporcional. Escaños en la Cámara de Diputados: 60 diputadas y diputados plurinominales, son elegidas y elegidos de las listas encabezadas por las candidaturas para presidenta o presidente del Estado, mediante el sistema proporcional. 63 diputadas y diputados uninominales, son elegidas y elegidos por simple mayoría de sufragios válidos. 7 diputadas y diputados circunscripciones especiales, corresponden a cada departamento donde habitan Naciones y Pueblos indígena Originario Campesinos que representan a la plurinacionalidad del Estado. 9 representantes ante organismos Parlamentarios Supraestatales, por organización política, 1 por departamento.",
     category: "Sistema Electoral"
   },
   {
@@ -101,6 +101,60 @@ export const faqData: FAQItem[] = [
     question: "En caso de segunda vuelta ¿Cuándo se realizaría?",
     answer: "En un plazo de 60 días después de la primera votación.",
     category: "Resultados"
+  },
+  {
+    id: 17,
+    question: "Distribución de escaños en el departamento de Cochabamba",
+    answer: "9 uninominales, 9 plurinominales, 1 especial, 4 senadores, 1 supraestatal.",
+    category: "Sistema Electoral"
+  },
+  {
+    id: 18,
+    question: "¿Cómo se distribuyen los 130 escaños de la Cámara de Diputados?",
+    answer: "60 diputadas y diputados plurinominales, 63 diputados y diputados uninominales, 7 diputados y diputadas circunscripciones. 5 Pando, 29 La paz, 19 Cochabamba, 29 Santa Cruz, 9 Oruro, 13 Potosí, 8 Beni, 9 Chuquisaca y 9 Tarija.",
+    category: "Sistema Electoral"
+  },
+  {
+    id: 19,
+    question: "¿Qué es el Padrón Electoral Biométrico?",
+    answer: "Es el sistema de registro biométrico de todas las bolivianas y bolivianos en edad de votar (mayores de 18 años) y de los extranjeros habilitados por ley para ejercer su derecho al voto en Bolivia.",
+    category: "Proceso Electoral"
+  },
+  {
+    id: 20,
+    question: "¿Qué datos se registran en el Padrón Electoral?",
+    answer: "El Padrón Electoral es el registro de identificación personal más completo de Bolivia, porque incluye datos biométricos y biográficos de las personas empadronadas: 10 huellas dactilares, Fotografía facial, Firma digitalizada.",
+    category: "Proceso Electoral"
+  },
+  {
+    id: 21,
+    question: "¿Tengo que llevar mi lapicero?",
+    answer: "Existencia en la mesa de sufragio.",
+    category: "Votación"
+  },
+  {
+    id: 22,
+    question: "¿Cómo funciona el voto asistido?",
+    answer: "El voto asistido es un derecho de las personas con discapacidad o mayores de 60 años que soliciten expresamente ayuda para votar. Las personas mayores de 60 años o con dificultades visuales, auditivas o para trasladarse que necesiten ayuda para votar pueden pedir asistencia a la presidenta o presidente de su mesa de votación. La presidenta o presidente de la mesa verifica la situación del elector y comunica su necesidad a los otros jurados y a los delegados de la mesa. Formas de voto asistido: La presidenta o presidente de la mesa acompaña a la persona al recinto y vigila que se cumpla la voluntad de la persona que solicitó el voto asistido. El acompañante marca las opciones que decida la electora o el elector. Si la electora o elector no tuviera un acompañante, la presidenta o presidente de mesa escoge a una persona de la la para que entre como testigo y luego marcará la papeleta siguiendo la voluntad de la electora o elector, en presencia del testigo. El material electoral para la asistencia a personas con discapacidad incluye: Una cercha en alfabeto braille, que se superpone a la papeleta de sufragio, en el caso de personas con dificultades visuales. Material impreso con información e instrucciones escritas sobre 'cómo votar' para personas con dificultades auditivas.",
+    category: "Votación"
+  },
+  {
+    id: 23,
+    question: "¿Qué es el certificado de sufragio?",
+    answer: "Es el documento público que certifica la participación de las personas en el acto electoral. Es la constancia de que la ciudadana o ciudadano votó. El certificado de sufragio, que se entrega a toda persona que ha votado, será válido siempre y cuando tenga la firma y huella dactilar de la presidenta o presidente de la mesa de sufragio.",
+    category: "Documentación"
+  },
+  {
+    id: 24,
+    question: "¿Qué faltas electorales existen para personas particulares y cuáles son sus sanciones?",
+    answer: "No votar el día de la elección o no exhibir el certificado de sufragio o de impedimento. Sanción: Multa por el equivalente de 10 % de un salario mínimo, caso contrario se aplicará el impedimento por 90 días para acceder a cargos públicos, efectuar trámites bancarios y obtener pasaporte. Quedan eximidas de esta sanción las personas que no votaron: Por caso fortuito o fuerza mayor comprobada documentalmente, Por enfermedad, acreditada por certificado médico de una entidad de salud, Por ser mayores de setenta (70) años, Por estar ausentes del territorio nacional el día de la votación. Incumplir resoluciones electorales, Obstaculizar por cualquier medio la representación que deban hacer ciudadanos inhabilitados ante la autoridad competente para su habilitación, Omisión su inscripción en el Padrón Electoral o inscribirse con datos incompletos, Circular en vehículos motorizados, sin la autorización respectiva. Sanción: Multa por el equivalente de entre 5 % y 20 % de un salario mínimo. Realizar cualquier acción sin el consentimiento de la persona, para revelar o difundir su voto, violando la protección del secreto del voto, Obstaculizar el ejercicio del control social, Incitar o realizar manifestaciones, reuniones o propaganda política en las proximidades de la mesa de sufragio o fuera de los plazos establecidos, Consumir bebidas alcohólicas en los plazos prohibidos por ley. Sanción: Multa por el equivalente a entre 21 % y 35 % de un salario mínimo. Vender o distribuir bebidas alcohólicas en los plazos prohibidos por ley. Sanción: Multa por el equivalente de entre 47 % y 60 % de un salario mínimo. Difundir resultados de estudios de opinión para fines electorales sin habilitación previa del Tribunal Electoral que corresponda, fuera de plazo o sin cumplir los criterios técnicos y metodológicos definidos por reglamento. Sanción: Multa por el equivalente de entre 47 % y 60 % de un salario mínimo. Portar armas de cualquier tipo, el día de elección. Sanción: Multa por el equivalente de entre 47 % y 60 % de un salario mínimo. De acuerdo a la Ley No 400, de 18/09/2013, en caso de configurar delito remisión al Ministerio Público, para el inicio del proceso penal.",
+    category: "Proceso Electoral"
+  },
+  {
+    id: 25,
+    question: "¿Cuáles son las causales de la nulidad de actas electorales?",
+    answer: "Se consideran causales de nulidad de un acta electoral las siguientes situaciones: a. La ausencia de las firmas y huellas dactilares de por lo menos tres Jurados Electorales legalmente designados. Se admitirá la impresión dactilar, sin firma, de una o un solo jurado. b. El uso de formularios de Actas no aprobados por la autoridad electoral competente. c. El funcionamiento de la mesa de sufragio en lugar distinto al señalado por la autoridad electoral competente. d. El funcionamiento de la mesa de sufragio en día distinto del fijado para el verificativo de la elección. e. El cómputo de votos emitidos en papeletas distintas a las proporcionadas por la autoridad electoral competente. f. El uso de papeletas de sufragio de distinta circunscripción uninominal o especial. g. La existencia de elementos que contradigan los datos contenidos en el Acta Electoral, aunque no se hubiere asentado la observación en el Acta. h. La existencia de datos asentados en el Acta Electoral que sean contradictorios o inconsistentes entre sí, aunque no se hubiere asentado la observación en el Acta. i. La existencia de diferencias en los datos del Acta Electoral original y sus copias, aunque no se hubiere asentado la observación en el Acta. j. La existencia de alteración de datos, borrones o tachaduras en el Acta Electoral, que no hayan sido señaladas en las observaciones de la propia Acta. k. La violación de la integridad del sobre de seguridad o el extravío del Acta original, cuando no pueda ser reemplazada por dos copias auténticas e iguales. l. La consignación de un número de votos en el Acta Electoral que supere la cantidad de personas inscritas en la mesa. Durante el proceso de valoración de la existencia de causales de nulidad, el Tribunal Electoral Departamental podrá revisar todo el material electoral contenido en los sobres de seguridad y, si fuera necesario, recurrir a las Notarías o Notarios y a las Juradas o Jurados de la mesa de sufragio correspondiente para solicitar aclaraciones. Declarada la nulidad, se repetirá el acto de votación en la mesa de sufragio correspondiente, con el mismo padrón y con nuevos jurados electorales, el segundo domingo siguiente de realizada la elección.",
+    category: "Documentación"
   }
 ];
 
