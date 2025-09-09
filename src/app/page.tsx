358240bc19fb37dbdb0acf75df667fc3d3b4bfbd
brayanaplm01 <apazalamas19@gmail.com>
2025-09-09 09:22:06 -0400

    Fix indicadores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@
 import { useState } from "react";
 import { HeroSection } from "./components/layouts/hero-section";
 import { SliderLogos } from "./components/layouts/slider-logos";
+import { QuienesSomosSection } from "./components/layouts/quienes-somos-section";
+import { AboutSection } from "./components/layouts/about-section";
+import MissionVisionSection from "./components/layouts/mission-vision-section";
 import { LatestNewsSection } from "./components/layouts/latest-news-section";
 import { HowToParticipateSection } from "./components/layouts/how-to-participate-section";
 import { PublicacionesCoalicionSeccion } from "./components/layouts/publicaciones-coalicion-seccion";
-
 import { Footer } from "./components/layouts/footer";
 import SimpleNavbar from "./components/layouts/simple-navbar";
 import { FAQSection } from "./components/layouts/FAQ";
@@ -29,7 +31,14 @@ export default function Home() {
           <HeroSection />
         </div>
         
-       
+        {/* Mission, Vision & Objectives Section - Nuestra Propuesta */}
+        <MissionVisionSection />
+        
+        {/* About Section - Quiénes Somos */}
+        <QuienesSomosSection />
+        
+        {/* Indicadores Section */}
+        <AboutSection />
         
         {/* Latest News Section - Últimas Noticias */}
         <LatestNewsSection />
